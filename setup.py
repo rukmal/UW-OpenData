3fcb4d5b7f39a8e63554840d5211c58cc6197f04
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-04-18 06:06:46 -0700

    Added setup file, made changes to directory file. Added Python 'requests' module to the project requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+	name='uwopendata',
+	version='1.0.0',
+	description='Open Python APIs for UW online services',
+	author='Amit Burstein and Rukmal Weerawarana',
+	packages=['uw']
+	)
--- uw/directory.py
+++ uw/directory.py
@@ -1,2 +1,12 @@
+import json
+from bs4 import BeautifulSoup
+import urllib2
+import urllib3
+
 class Directory(object):
-	
+'''
+Author: Rukmal Weerawarana
+Description: API to get data from the University of Washington official directory.
+'''
+	def __init__(self):
+		self.DIRECTORY_URL = 'http://washington.edu/home/peopledir'
